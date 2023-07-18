0134cc04cd2b882a77abd68bd3a6b7a05a5ac7ca
ParbhatKataria <parbhatkataria628@gmail.com>
2023-07-18 20:31:36 +0530

    fix:fixed the search bar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,30 @@
 import "./App.css";
 import { useQuery } from "react-query";
 import axios from "axios";
-import {  Box, Grid } from "@chakra-ui/react";
+import {  Box, Grid, Input } from "@chakra-ui/react";
 import CardItem from "./component/Card";
-
+import { useState } from "react";
+const url = 'https://youtube-server-vmap.onrender.com';
+// const url = 'http://localhost:4500'
 function App() {
-  const { isLoading, error, data } = useQuery("data", () =>
-    axios.get(`https://youtube-server-vmap.onrender.com`).then((res) => res.data)
+  const [search, setsearch] = useState('');
+  const { isLoading, error, data } = useQuery(["data", search], (search) =>{
+    const data = search.queryKey[1];
+    return axios.get(`${url}/search?search=${data}`).then(res=>res.data)
+  }
+  
   );
-  if (isLoading) return "Loading...";
+  console.log(data)
   if (error) return "An error has occurred: " + error.message;
   return (
-    <Box className="App">
-      <Grid w={'90%'} margin={'auto'} templateColumns='repeat(3, 1fr)' gap={10}>
-        {data.length && data.map((el) => {
+    <Box w={'90%'} margin={'auto'} className="App">
+      <Box my='20px'>
+        <Input onChange={(e)=>{setsearch(e.target.value)}}  placeholder='Seach By title or description'></Input>
+      </Box>
+      {isLoading && <Box>Loading...</Box>}
+
+      <Grid  templateColumns='repeat(3, 1fr)' gap={10}>
+        {data?.length && data.map((el) => {
           return (
             <CardItem key={el._id}  data={el}/>
           );
